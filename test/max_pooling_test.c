3e1e799c4a947d0fb4862af0d02b3341f362565c
Konstantin Lübeck <konstantin.luebeck@student.uni-tuebingen.de>
2017-09-04 15:26:16 +0200

    Updated max_pooling_test in order to work with updated read_pgm().

--- test/max_pooling_test.c
+++ test/max_pooling_test.c
@@ -3,6 +3,7 @@
  *
  * @author Konstantin Luebeck (University of Tuebingen, Chair for Embedded Systems)
  */
+#define DEBUG
 #include "pico-cnn/pico-cnn.h"
 
 void usage() {
@@ -15,16 +16,14 @@ int main(int argc, char** argv) {
         usage();
     }
 
+    uint16_t height, width;
     float_t* input_image;
 
-    if(read_pgm(&input_image, argv[1], 0, 0.0, 1.0) != 0) {
+    if(read_pgm(&input_image, argv[1], 0, 0.0, 1.0, &height, &width) != 0) {
         fprintf(stderr, "could not read pgm image '%s'!\n", argv[1]);
         return 1;
     }
 
-    uint16_t height, width;
-    height = 32;
-    width = 32;
 
     float_t* output_image = (float_t*) malloc((height/2) * (width/2) * sizeof(float_t));
 
