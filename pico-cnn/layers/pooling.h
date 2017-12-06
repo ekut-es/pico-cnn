001ffd2021d82334a94b642c91a78f48080cc737
Konstantin Lübeck <konstantin.luebeck@student.uni-tuebingen.de>
2017-12-06 09:31:02 +0100

    Refined max_pooling2d_naive(..) to use a stride different from the kernel_size.

--- pico-cnn/layers/pooling.h
+++ pico-cnn/layers/pooling.h
@@ -19,26 +19,37 @@
  * @param new_image (height/kernel_size x width/kernel_size)
  * @param kernel_size
  */
-void max_pooling2d_naive(const fp_t* original_image, const uint16_t height, const uint16_t width, fp_t* new_image, const uint16_t kernel_size) {
+void max_pooling2d_naive(const fp_t* original_image, const uint16_t height, const uint16_t width, fp_t* new_image, const uint16_t kernel_size, const uint16_t stride) {
 
-    uint16_t row, column;
+    uint16_t image_row, image_column;
+    uint16_t new_image_row, new_image_column;
+    uint16_t new_image_width;
 
-    for(row = 0; row < height; row += kernel_size) {
-        for(column = 0; column < width; column += kernel_size) {
-            fp_t pixel = original_image[row*width+column];
+    uint16_t kernel_row, kernel_column;
+
+    new_image_row = 0;
+    new_image_column = 0;
     
-            uint16_t sub_row, sub_column;
+    new_image_width = width/stride;
+
+    for(image_row = 0; image_row < height; image_row += stride) {
+        for(image_column = 0; image_column < width; image_column += stride) {
+            fp_t pixel = original_image[image_row*width+image_column];
+    
             
-            for(sub_row = row; sub_row < row+kernel_size; sub_row++) {
-                for(sub_column = column; sub_column < column+kernel_size; sub_column++) {
-                    if(original_image[sub_row*width+sub_column] > pixel) {
-                        pixel = original_image[sub_row*width+sub_column];
+            for(kernel_row = image_row; kernel_row < image_row+kernel_size; kernel_row++) {
+                for(kernel_column = image_column; kernel_column < image_column+kernel_size; kernel_column++) {
+                    if(original_image[kernel_row*width+kernel_column] > pixel) {
+                        pixel = original_image[kernel_row*width+kernel_column];
                     }
                 }
             }
             
-            new_image[(row/kernel_size)*(height/kernel_size)+(column/kernel_size)] = pixel;
+            new_image[new_image_row*new_image_width+new_image_column] = pixel;
+            new_image_column++;
         }
+        new_image_row++;
+        new_image_column = 0;
     }
 }
 
