451a4d806986ae0e9564affa7f9d786b4eee0641
Konstantin Lübeck <konstantin.luebeck@student.uni-tuebingen.de>
2017-12-06 09:06:22 +0100

    Added local response normalization layer.

--- pico-cnn/parameters.h
+++ pico-cnn/parameters.h
@@ -7,9 +7,11 @@
 #ifndef PARAMETERS_H
 #define PARAMETERS_H
 
-//typedef float fp_t;
 typedef float fp_t;
 extern fp_t max_float;
 extern fp_t min_float;
+
+#define MAX(x, y) (((x) > (y)) ? (x) : (y))
+#define MIN(x, y) (((x) < (y)) ? (x) : (y))
 
 #endif // PARAMETERS_H
--- pico-cnn/pico-cnn.h
+++ pico-cnn/pico-cnn.h
@@ -13,6 +13,7 @@
 #include "layers/convolution.h"
 #include "layers/pooling.h"
 #include "layers/fully_connected.h"
+#include "layers/local_response_normalization.h"
 
 #include "io/read_weights.h"
 #include "io/read_pgm.h"
