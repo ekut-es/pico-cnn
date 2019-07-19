1d5cd80cf8e3fde354c760204fa570cc131a8478
Alexander Jung <a.jung@uni-tuebingen.de>
2019-07-19 16:25:13 +0200

    Added code template for LRN

---
+++ onnx_import/code_templates/pico_cnn_lrn.c
@@ -0,0 +1,2 @@
+local_response_normalization_naive({{input_buffer.name}}, {{height}}, {{width}}, {{depth}},
+                                   {{output_buffer.name}}, {{alpha}}, {{beta}}, {{size}});
