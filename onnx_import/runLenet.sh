737cc15333a517d94321b81491a3693e284c612f
Nils Weinhardt <nils.weinhardt@student.uni-tuebingen.de>
2019-10-20 16:08:20 +0200

    removed unused shell skript

--- onnx_import/runLenet.sh
+++
@@ -1,5 +0,0 @@
-cd /local/weinnils/git/pico-cnn/onnx_import 
-python3.6 onnx_to_pico_cnn.py --input /nfs/es-genial/pico-cnn/data/onnx/lenet.onnx
-cd generated_code/lenet
-make lenet
-./lenet /local/weinnils/git/mnist network.weights.bin 
