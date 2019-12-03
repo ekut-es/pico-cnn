61d0697a7f807858dd901dd573bb320f714d6009
Nils Weinhardt <nils.weinhardt@student.uni-tuebingen.de>
2019-12-03 13:29:00 +0100

    Changed Makefile: tests/layers/activation_function is now included. Changed run_tests: the test for ReLU is now included

--- test/run_tests.c
+++ test/run_tests.c
@@ -2,6 +2,7 @@
 #include <assert.h>
 
 #include "layers/test_pooling.h"
+#include "layers/test_activation_function.h"
 
 int main() {
 
@@ -16,6 +17,9 @@ int main() {
     assert(test_avg_pooling2d() == 0);
     assert(test_avg_pooling2d_padding() == 0);
     printf("Success: pooling operations!\n");
+
+    assert(test_relu_naive() == 0);
+    printf("Success: activation functions! :) \n");
 
     printf("All tests successful!\n");
 
