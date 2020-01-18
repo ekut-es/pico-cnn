f65146533abc00f88f5ca12088988b1804b07917
Nils Weinhardt <nils.weinhardt@student.uni-tuebingen.de>
2020-01-18 16:36:49 +0100

    added test for 2D concatenation.

---
+++ test/layers/test_concatenate.c
@@ -0,0 +1,48 @@
+#include "test_concatenate.h"
+
+int test_concatenate_2D() {
+
+    printf("test_concatenate_2D()\n");
+    int return_value = 0;
+
+    #define input_width 3
+    #define input_height 2
+    #define num_inputs 4
+
+    fp_t error = 0.0001;
+    int dimension = 0;
+
+    fp_t input1[input_width*input_height] = {3, -9, -16,
+                                             14, -18, 19};
+    fp_t input2[input_width*input_height] = {-9, 10, -6,
+                                              5, -19, -6};
+    fp_t input3[input_width*input_height] = {5, -17, 5,
+                                            -5, -13, -4};
+    fp_t input4[input_width*input_height] = {12, 10, 15,
+                                            -11, -6, -6};
+    fp_t* input[num_inputs] = {input1, input2, input3, input4};
+
+    fp_t expected_output[input_width*input_height*num_inputs] =
+        {3, -9, -16,
+        14, -18, 19,
+        -9, 10, -6,
+         5, -19, -6,
+         5, -17, 5,
+        -5, -13, -4,
+        12, 10, 15,
+        -11, -6, -6};
+
+    fp_t* output = (fp_t*) malloc(input_width * input_height * num_inputs * sizeof(fp_t));
+
+    concatenate_2D(input, input_width, input_height, dimension, num_inputs, output);
+
+    return_value = compare1dFloatArray(output, expected_output, input_width*input_height, error);
+    print1dFloatArray_2d(output,8,2);
+
+    free(output);
+    return return_value;
+
+    #undef input_width
+    #undef input_height
+    #undef num_inputs
+}
---
+++ test/layers/test_concatenate.h
@@ -0,0 +1,14 @@
+#ifndef TEST_CONCATENATE_H
+#define TEST_CONCATENATE_H
+
+#include "pico-cnn/parameters.h"
+#include "pico-cnn/layers/concatenate.h"
+#include "../utility_functions.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+int test_concatenate_2D();
+
+#endif //TEST_CONCATENATE_H
