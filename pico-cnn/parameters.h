bad3e11bb864ffd6e025facd685ad6c6a29f4e4c
Alexander Jung <a.jung@uni-tuebingen.de>
2020-03-04 10:33:46 +0100

    Fixed DEBUG_MSG and added INFO_MSG and ERROR_MSG.

--- pico-cnn/parameters.h
+++ pico-cnn/parameters.h
@@ -17,12 +17,20 @@ extern fp_t min_float;
 #ifdef DEBUG
 #define DEBUG_MSG(...) fprintf( stderr, __VA_ARGS__ );
 #else
-# define DEBUG_MSG(...) do{ } while ( false );
+#define DEBUG_MSG(...) do{ } while ( 0 );
 #endif
 
-#define FLOAT_MIN -100000
+#ifdef INFO
+#define INFO_MSG(...) fprintf( stdout, __VA_ARGS__ );
+#else
+#define INFO_MSG(...) do{ } while ( 0 );
+#endif
+
+#define ERROR_MSG(...) fprintf( stderr, __VA_ARGS__ );
 
 #define MAX(x, y) (((x) > (y)) ? (x) : (y))
 #define MIN(x, y) (((x) < (y)) ? (x) : (y))
+
+#define FLOAT_MIN -100000
 
 #endif // PARAMETERS_H
