e62f43c422bdd21c0d57c984ec63ca90608d27c1
Alexander Jung <a.jung@uni-tuebingen.de>
2020-04-23 10:55:25 +0200

    Removed random function as it is not a part of the library.

--- pico-cnn-cpp/utils.h
+++ pico-cnn-cpp/utils.h
@@ -12,8 +12,4 @@ inline bool fp_t_eq(fp_t A, fp_t B, fp_t epsilon = EPSILON)
     return (std::fabs(A - B) < epsilon);
 }
 
-static inline fp_t urand(fp_t min, fp_t max) {
-    return (fp_t) ((((fp_t)rand()/(fp_t)(RAND_MAX)) * 1.0f) * (max - min) + min);
-}
-
 #endif // UTILS_H
