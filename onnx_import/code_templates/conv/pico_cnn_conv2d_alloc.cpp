0c3382eb9bc45fcf9cd01c8681d36b167348c47e
Alexander Jung <a.jung@uni-tuebingen.de>
2020-05-15 16:38:16 +0200

    Fixed Convolution code template.

--- onnx_import/code_templates/conv/pico_cnn_conv2d_alloc.cpp
+++ onnx_import/code_templates/conv/pico_cnn_conv2d_alloc.cpp
@@ -4,12 +4,24 @@
     uint32_t {{identifier}}_groups = {{num_groups}};
 
     {{identifier}}_layer = new pico_cnn::naive::Convolution("{{name}}", 0, pico_cnn::op_type::Conv,
-                                                   {{kernel.name}}, {{bias_buffer.name}}, {{identifier}}_padding, {{identifier}}_stride, {{identifier}}_groups);
+                                                   {{kernel.name}},
+                                                   {% if bias_buffer %}
+                                                   {{bias_buffer.name}},
+                                                   {% else %}
+                                                   nullptr,
+                                                   {% endif %}
+                                                   {{identifier}}_padding, {{identifier}}_stride, {{identifier}}_groups);
 {% else %}
     uint32_t {{identifier}}_stride[2] = { {{stride.0}}, {{stride.1}} };
     uint32_t {{identifier}}_groups = {{num_groups}};
 
     {{identifier}}_layer = new pico_cnn::naive::Convolution("{{name}}", 0, pico_cnn::op_type::Conv,
-                                                   {{kernel.name}}, {{bias_buffer.name}}, nullptr, {{identifier}}_stride, {{identifier}}_groups);
+                                                   {{kernel.name}},
+                                                   {% if bias_buffer %}
+                                                   {{bias_buffer.name}},
+                                                   {% else %}
+                                                   nullptr,
+                                                   {% endif %}
+                                                   nullptr, {{identifier}}_stride, {{identifier}}_groups);
 {% endif %}
 
