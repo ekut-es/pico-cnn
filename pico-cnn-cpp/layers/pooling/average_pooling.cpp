68ea0a4cf9b5318d47128802dff0a2404b0c99c3
Alexander Jung <a.jung@uni-tuebingen.de>
2020-06-25 11:53:51 +0200

    Fixed bug in AveragePooling when count_include_pad=1 and padding is asymmetric.

--- pico-cnn-cpp/layers/pooling/average_pooling.cpp
+++ pico-cnn-cpp/layers/pooling/average_pooling.cpp
@@ -73,8 +73,6 @@ void pico_cnn::naive::AveragePooling::pool(pico_cnn::naive::Tensor *input, pico_
 
     } else if(count_include_pad_ == 0) {
 
-        uint32_t crop[2] = {kernel_size_[0]/2, kernel_size_[1]/2};
-
         for (uint32_t batch = 0; batch < num_batches; batch++) {
             for (uint32_t channel = 0; channel < num_channels; channel++) {
 
@@ -101,49 +99,68 @@ void pico_cnn::naive::AveragePooling::pool(pico_cnn::naive::Tensor *input, pico_
                             }
                         }
 
-                        // center case
-                        if (output_channel_row >= crop[0] && output_channel_row < output_channel_height - crop[0] &&
-                            output_channel_column >= crop[1] && output_channel_column < output_channel_width - crop[1]) {
+                        if (num_dims == 4) {
+                            // center case
+                            if (output_channel_row >= padding_[0] && output_channel_row < output_channel_height - padding_[2] &&
+                                output_channel_column >= padding_[1] && output_channel_column < output_channel_width - padding_[3]) {
 
-                            if (num_dims == 4) {
                                 output->access(batch, channel, output_channel_row, output_channel_column,
                                                num_channels, output_height, output_width) = pixel / ((fp_t) (kernel_size_[0] * kernel_size_[1]));
-                            } else if (num_dims == 3) {
-                                output->access(batch, channel, output_channel_column,
-                                               num_channels, output_width) = pixel / ((fp_t) (kernel_size_[0] * kernel_size_[1]));
-                            }
 
-                        // edge case
-                        } else {
+                            // edge case
+                            } else {
 
-                            uint32_t up_row, down_row, left_col, right_col;
-                            int32_t divisor, div_row, div_col;
+                                uint32_t up_row, down_row, left_col, right_col;
+                                int32_t divisor, div_row, div_col;
 
-                            up_row = MAX(channel_row, crop[0]);
-                            down_row = MIN(channel_row + kernel_size_[0] - 1, height - crop[0] - 1);
-                            div_row = down_row - up_row + 1;
+                                up_row = MAX(channel_row, padding_[0]);
+                                down_row = MIN(channel_row + kernel_size_[0] - 1, height - padding_[2] - 1);
+                                div_row = down_row - up_row + 1;
 
-                            left_col = MAX(channel_column, crop[1]);
-                            right_col = MIN(channel_column + kernel_size_[1] - 1, width - crop[1] - 1);
-                            div_col = right_col - left_col + 1;
+                                left_col = MAX(channel_column, padding_[1]);
+                                right_col = MIN(channel_column + kernel_size_[1] - 1, width - padding_[3] - 1);
+                                div_col = right_col - left_col + 1;
 
-                            divisor = div_row * div_col;
+                                divisor = div_row * div_col;
 
-                            if (divisor == 0) {
-                                PRINT_ERROR("ERROR: Division by zero! Aborting execution.")
-                                exit(1);
-                            }
+                                if (divisor == 0) {
+                                    PRINT_ERROR("ERROR: Division by zero! Aborting execution.")
+                                    exit(1);
+                                }
 
-                            if (num_dims == 4) {
                                 output->access(batch, channel, output_channel_row, output_channel_column,
                                                num_channels, output_height, output_width) = pixel / ((fp_t) (divisor));
-                            } else if (num_dims == 3) {
+
+
+                            }
+                        } else if (num_dims == 3) {
+                            // center case
+                            if (output_channel_row >= padding_[0] && output_channel_row < output_channel_height - padding_[2] &&
+                                output_channel_column >= padding_[1] && output_channel_column < output_channel_width - padding_[3]) {
+
+                                output->access(batch, channel, output_channel_column,
+                                               num_channels, output_width) = pixel / ((fp_t) (kernel_size_[0] * kernel_size_[1]));
+                            // edge case
+                            } else {
+
+                                uint32_t left_col, right_col;
+                                int32_t divisor, div_col;
+
+                                left_col = MAX(channel_column, padding_[0]);
+                                right_col = MIN(channel_column + kernel_size_[1] - 1, width - padding_[1] - 1);
+                                div_col = right_col - left_col + 1;
+
+                                divisor = div_col;
+
+                                if (divisor == 0) {
+                                    PRINT_ERROR("ERROR: Division by zero! Aborting execution.")
+                                    exit(1);
+                                }
+
                                 output->access(batch, channel, output_channel_column,
                                                num_channels, output_width) = pixel / ((fp_t) (divisor));
                             }
-
                         }
-
 
                         output_channel_column++;
                     }
